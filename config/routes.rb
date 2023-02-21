715c0bc37b78e672c736684ca9f6234dcf6bafdc
Lara Franchi <laradeandrade@protonmail.com>
2023-02-21 15:39:11 -0500

    TaskManagerRails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Add new route to list the tasks
+  get '/tasks', to: 'tasks#index'
 end
